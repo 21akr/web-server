9a15f5c3359f7b2dceda67f5bc1ff36f6d0b927c
Akramhujaev Mukhammadrasul <it21akr@gmail.com>
2024-04-22 14:52:34 +0500

    update for deployment

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 export const db = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.CUSTOM_DATABASE_URL,
   // user: process.env.PGUSER,
   //   host: process.env.PGHOST,
   //   database: process.env.PGDATABASE,
