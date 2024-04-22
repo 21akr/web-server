63bdc97b84e2648d1e262c28e9d9dd2ce0ef5cf0
Akramhujaev Mukhammadrasul <it21akr@gmail.com>
2024-04-22 16:55:01 +0500

    update for deployment

--- src/controllers/UserPurchaseSkin.controller.ts
+++ src/controllers/UserPurchaseSkin.controller.ts
@@ -15,7 +15,7 @@ export async function UserPurchaseSkinController(req: Request, res: Response) {
     if (price !== null && userBalance >= price) {
       const newBalance = userBalance - price;
       await UserService.updateBalance(userId, newBalance);
-      res.send(`An item for ${price} EUR is purchased!\nYour balance is: ${userBalance}`)
+      res.send(`An item for ${price} EUR is purchased!\nYour balance is: ${newBalance}`)
     } else {
       res.status(400).send('Insufficient funds');
     }
