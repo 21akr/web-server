a38bb73f7f7ca0b7a332fe2890235d9780d45596
Akramhujaev Mukhammadrasul <it21akr@gmail.com>
2024-05-02 15:35:45 +0500

    update for deployment

--- src/controllers/UserPurchaseSkin.controller.ts
+++ src/controllers/UserPurchaseSkin.controller.ts
@@ -2,24 +2,21 @@ import { Request, Response } from 'express';
 import { SkinportService, UserService } from '../services';
 
 export async function UserPurchaseSkinController(req: Request, res: Response) {
-  const userId: number = Number(req.query.userId);
-  const itemId: number = Number(req.query.itemId);
+  const userId = Number(req.query.userId);
+  const itemId = Number(req.query.itemId);
 
   try {
-    const userBalance = await UserService.getBalance(userId);
+    const userBalanceCents = await UserService.getBalance(userId);
     const item = await SkinportService.getItemById(itemId);
     const price = item?.min_price;
 
     if (!item) throw new Error('Item not found');
 
-    if (price !== undefined && userBalance >= price) {
-      const newBalance = userBalance - price;
-      if (newBalance >= 0) {
-        await UserService.updateBalance(userId, newBalance);
-        res.send(`An item for ${price} EUR is purchased!\nYour balance is: ${newBalance}`);
-      } else {
-        res.status(400).send('Insufficient funds');
-      }
+    if (price !== undefined && userBalanceCents >= price) {
+      const newBalanceCents = userBalanceCents - price;
+      await UserService.updateBalance(userId, newBalanceCents);
+      const newBalance = newBalanceCents / 100;
+      res.send(`An item for ${price / 100} EUR is purchased!\nYour balance is: ${newBalance} EUR`);
     } else {
       res.status(400).send('Insufficient funds');
     }
--- src/services/User.service.ts
+++ src/services/User.service.ts
@@ -2,11 +2,11 @@ import { db } from './Postgres.service';
 
 export class UserService {
   static async getBalance(_id: number): Promise<number> {
-    const result = await db.query(`SELECT balance FROM users WHERE id = $1`, [_id]);
-    return result.rows[0].balance;
+    const result = await db.query('SELECT balance FROM users WHERE id = $1', [_id]);
+    return result.rows[0]?.balance || 0;
   }
 
   static async updateBalance(_id: number, newBalance: number): Promise<void> {
-    await db.query(`UPDATE users SET balance = $1 WHERE id = $2`, [newBalance, _id]);
+    await db.query('UPDATE users SET balance = $1 WHERE id = $2', [newBalance, _id]);
   }
 }
