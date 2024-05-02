e25055661692d2df8153574e48046ce378d3429d
Akramhujaev Mukhammadrasul <it21akr@gmail.com>
2024-05-02 16:40:58 +0500

    update for deployment

--- src/controllers/UserPurchaseSkin.controller.ts
+++ src/controllers/UserPurchaseSkin.controller.ts
@@ -4,24 +4,20 @@ import { SkinportService, UserService } from '../services';
 export async function UserPurchaseSkinController(req: Request, res: Response) {
   const userId = Number(req.query.userId);
   const itemId = Number(req.query.itemId);
-  console.log(userId);
-  console.log(itemId);
-
-  console.log(typeof userId);
-  console.log(typeof itemId);
 
   try {
     const userBalanceCents = await UserService.getBalance(userId);
     const item = await SkinportService.getItemById(itemId);
     const price = item?.min_price;
-    console.log('price, ', price);
 
     if (!item) throw new Error('Item not found');
 
     if (price !== undefined && userBalanceCents >= price) {
       const newBalanceCents = userBalanceCents - price;
+      console.log(newBalanceCents);
       await UserService.updateBalance(userId, newBalanceCents);
       const newBalance = newBalanceCents / 100;
+      console.log(newBalance);
       res.send(`An item for ${price / 100} EUR is purchased!\nYour balance is: ${newBalance} EUR`);
     } else {
       res.status(400).send('Insufficient funds');
